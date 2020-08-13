c75424af9730e70263fb2dbaf0d7cd5ada3d6113
Your name here <innocentleonard0007@gmail.com>
2020-08-13 18:21:16 +0300

    another model

--- models/Author.js
+++ models/Author.js
@@ -1,8 +1,46 @@
-require ('mongoose')
+const mongoose = require ('mongoose')
+const validator = require('validator')
 
-const Author = mongoose.model('Author', {
-    first_name :{},
-    last_name:{},
-    address: {},
-    email: {},
+
+const authorSchema = new mongoose.Schema({
+    full_name :{
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+          if (!validator.isEmail(value)){
+                 throw new Error ('Email is invalid')
+          }
+        }
+    },
+    password: {
+        type: String,
+        trim:true,
+        minlength: 8,
+        required: true
+    },
+
+    tokens: [{
+        token : {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+const Author = mongoose.model('Author', authorSchema)
+
+
+module.exports = Author
--- models/Publisher.js
+++ models/Publisher.js
@@ -1,8 +1,49 @@
-require ('mongoose')
+const mongoose = require ('mongoose')
+const validator = require('validator')
 
-const Publisher = mongoose.model('Publisher', {
-    company_name: {},
-    address: {},
-    email: {},
-    tokens:[{}]
+const pubSchema = new mongoose.Schema({
+    company_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+          if (!validator.isEmail(value)){
+                 throw new Error ('Email is invalid')
+          }
+        }
+    },
+    password: {
+        type: String,
+        trim:true,
+        minlength: 8,
+        required: true
+    },
+
+    tokens: [{
+        token : {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+const Publisher = mongoose.model('Publisher', pubSchema)
+
+module.exports = Publisher
